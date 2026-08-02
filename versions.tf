ccc1f672d95f04801ee5f02ee05a3f9fadddfbdb
Jimmy AO <jimmy@archnops.com>
2026-08-03 00:40:50 +0200

    Initial commit.

---
+++ versions.tf
@@ -0,0 +1,13 @@
+# versions.tf
+
+
+terraform {
+  required_version = ">= 1.15.8"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "=6.57.1"
+    }
+  }
+}
